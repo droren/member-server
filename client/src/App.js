ac00f7d00b462e16e303d884f0f9cce65fc54559
Dennis Hjort <hjort.dennis@gmail.com>
2024-07-13 12:42:11 +0200

    Fixing bug in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
-import Register from './components/RegisterUser';
+import RegisterUser from './components/RegisterUser';
 import MemberList from './components/MemberList';
 import AddMember from './components/AddMember';
 import RemoveMember from './components/RemoveMember';
