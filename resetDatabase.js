d7d547d91a83d9b08aa27c184750a3d063132ea9
Dennis Hjort <hjort.dennis@gmail.com>
2024-07-13 13:20:45 +0200

    Fixing editmember.

--- resetDatabase.js
+++ resetDatabase.js
@@ -1,31 +1,39 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const User = require('./models/User').default;
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
 const Member = require('./models/Member');
 
-mongoose.connect('mongodb://localhost:27017/memberDB', {
+const saltRounds = 10;
+
+const mongoDBServerIP = process.env.MONGODB_SERVER_IP || 'localhost';
+const mongoDBDatabase = process.env.MONGODB_DATABASE || 'yourDatabase';
+
+mongoose.connect(`mongodb://${mongoDBServerIP}:27017/${mongoDBDatabase}`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true  
+  useUnifiedTopology: true
 });
 
-const resetDatabase = async () => {
+mongoose.connection.on('connected', async () => {
   try {
+    // Clear existing data
     await User.deleteMany({});
     await Member.deleteMany({});
 
-    const defaultUsername = 'goranadmin';
-    const defaultPassword = 'skogsrojet2012';
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash('skogsrojet2012', saltRounds);
 
-    const hashedPassword = await bcrypt.hash(defaultPassword, 10);
-    const defaultUser = new User({ username: defaultUsername, password: hashedPassword });
-    await defaultUser.save();
+    // Add default admin user
+    const adminUser = new User({
+      username: 'göranadmin',
+      password: hashedPassword
+    });
+    await adminUser.save();
 
     console.log('Database reset and default user created');
-    mongoose.disconnect();
+    mongoose.connection.close();
   } catch (err) {
     console.error('Error resetting database:', err);
-    mongoose.disconnect();
+    mongoose.connection.close();
   }
-};
-
-resetDatabase();
+});
